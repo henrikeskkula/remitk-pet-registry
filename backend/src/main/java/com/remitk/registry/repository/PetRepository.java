4798240dcd12f1738ddfd4d0d12f6b6ed666f937
M-Sirelbu <maivosirelbu@protonmail.com>
2026-03-18 16:30:27 +0200

    Added Search Pets endpoint
    Added Search Pets endpoint with pagination and sorting. Logic in PetController, PetService, PetRepository

--- backend/src/main/java/com/remitk/registry/repository/PetRepository.java
+++ backend/src/main/java/com/remitk/registry/repository/PetRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PetRepository extends JpaRepository<Pet, Long> {
     Page<Pet> findByNameContainingIgnoreCase(String name, Pageable pageable);
     Page<Pet> findByOwnerId(Long id, Pageable pageable);
+    Page<Pet> findByMicrochipId(Long id, Pageable pageable);
 }
