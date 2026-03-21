fc4142c5d67a465b4a5347943cd7b27c7545b675
M-Sirelbu <maivosirelbu@protonmail.com>
2026-03-21 09:36:30 +0200

    Add no-args-constructor and setter to MicrochipDTO and EditMicrochipStatusDTO

--- backend/src/main/java/com/remitk/registry/dto/EditMicrochipStatusDTO.java
+++ backend/src/main/java/com/remitk/registry/dto/EditMicrochipStatusDTO.java
@@ -5,11 +5,15 @@ import jakarta.validation.constraints.NotNull;
 
 public class EditMicrochipStatusDTO {
     @NotNull
-    private final MicrochipStatus status;
+    private MicrochipStatus status;
     public EditMicrochipStatusDTO(MicrochipStatus status) {
         this.status = status;
     }
+    public EditMicrochipStatusDTO() {}
     public MicrochipStatus getStatus() {
         return status;
+    }
+    public void setStatus(MicrochipStatus status) {
+        this.status = status;
     }
 }
--- backend/src/main/java/com/remitk/registry/dto/MicrochipDTO.java
+++ backend/src/main/java/com/remitk/registry/dto/MicrochipDTO.java
@@ -8,12 +8,12 @@ public class MicrochipDTO {
     private Long id;
     @NotNull
     @Size(max = 50)
-    private final String chipNumber;
+    private String chipNumber;
     @NotNull
     @Size(max = 255)
-    private final String importer;
+    private String importer;
     @NotNull
-    private final MicrochipStatus status;
+    private MicrochipStatus status;
 
     public MicrochipDTO(Long id, String chipNumber, String importer, MicrochipStatus status) {
         this.id = id;
@@ -21,6 +21,8 @@ public class MicrochipDTO {
         this.importer = importer;
         this.status = status;
     }
+
+    public MicrochipDTO() {}
 
     public Long getId() {
         return id;
@@ -40,5 +42,17 @@ public class MicrochipDTO {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public void setChipNumber(String chipNumber) {
+        this.chipNumber = chipNumber;
+    }
+
+    public void setImporter(String importer) {
+        this.importer = importer;
+    }
+
+    public void setStatus(MicrochipStatus status) {
+        this.status = status;
     }
 }
